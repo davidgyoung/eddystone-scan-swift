8b6986d794916d09b7d6a313a493a0f2e6c4a0c5
David G. Young <davidgyoung@gmail.com>
2019-09-16 15:47:22 -0400

    Initial Commit

---
+++ EddystoneDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EddystoneDetector
+//
+//  Created by David G. Young on 9/16/19.
+//  Copyright © 2019 david. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
